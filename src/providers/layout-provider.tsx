e3b00366e5e05a9558e2f86a09b6806ff00ab96a
abdulkareemm <eng.kreem.96@gmail.com>
2024-02-18 22:30:36 +0300

    separate menu to user and admin

--- src/providers/layout-provider.tsx
+++ src/providers/layout-provider.tsx
@@ -3,15 +3,54 @@ import { UserButton } from "@clerk/nextjs";
 import React, { useEffect, useState } from "react";
 import { GetCurrentUserFromMongoDB } from "../actions/users";
 import { User } from "@prisma/client";
-import { message } from "antd";
-import { usePathname } from "next/navigation";
+import { Button, Dropdown, message } from "antd";
+import { usePathname, useRouter } from "next/navigation";
 import Loader from "../components/Loader";
+
+const userMenu = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Properties",
+    path: "/user/properties",
+  },
+  {
+    name: "Account",
+    path: "/user/account",
+  },
+  {
+    name: "Subscriptions",
+    path: "/user/subscriptions",
+  },
+  {
+    name: "Queries",
+    path: "/user/queries",
+  },
+];
+const adminMenu = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Properties",
+    path: "/admin/properties",
+  },
+  {
+    name: "Users",
+    path: "/admin/users",
+  },
+];
 
 export default function LayoutProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const [menuToShow = userMenu, setMenuToShow] = React.useState<any>(userMenu);
   const [currentUserData = null, setCurrentUserData] =
     React.useState<User | null>(null);
   const [loading = false, setLoading] = React.useState<boolean>(false);
@@ -21,16 +60,38 @@ export default function LayoutProvider({
     if (isPublicRoute) return null;
     return (
       <div className="lg:px-20 px-5">
-        <div className="bg-primary p-4 flex justify-between items-center rounded-b">
-          <h1 className="text-white text-xl">AKM Properties</h1>
+        <div className="bg-primary p-3 flex justify-between items-center rounded-b">
+          <h1
+            className="text-2xl text-white font-bold cursor-pointer"
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            AKM Properties
+          </h1>
+
           <div className="bg-white py-2 px-5 rounded-sm flex items-center gap-5">
-            <span>{currentUserData?.username}</span>
+            <Dropdown
+              menu={{
+                items: menuToShow.map((item: any) => ({
+                  label: item.name,
+                  onClick: () => {
+                    router.push(item.path);
+                  },
+                })),
+              }}
+            >
+              <Button className="text-primary hover:text-primary" type="link">
+                {currentUserData?.username}
+              </Button>
+            </Dropdown>
             <UserButton afterSignOutUrl="/sign-in" />
           </div>
         </div>
       </div>
     );
   };
+
   const getContent = () => {
     if (isPublicRoute) return children;
     if (loading) return <Loader />;
@@ -42,6 +103,9 @@ export default function LayoutProvider({
       const response: any = await GetCurrentUserFromMongoDB();
       if (response.error) throw new Error(response.error.message);
       setCurrentUserData(response.data);
+      if (response.data.isAdmin) {
+        setMenuToShow(adminMenu);
+      }
     } catch (error: any) {
       console.log(error);
       message.error(error.message);
