3897481d17135a6b1deba013605203b117cadd8f
abdulkareemm <eng.kreem.96@gmail.com>
2024-02-19 11:17:48 +0300

    media step

--- src/app/(private)/user/properties/_components/properties-form/media.tsx
+++ src/app/(private)/user/properties/_components/properties-form/media.tsx
@@ -1,22 +1,78 @@
-import React from 'react'
-import { PropertiesFormStepProps } from '.';
-import { Button } from 'antd';
+import React from "react";
+import { PropertiesFormStepProps } from ".";
+import { Button, Upload } from "antd";
 
-export default function Media({currentStep,setCurrentStep}: PropertiesFormStepProps) {
+function Media({
+  currentStep,
+  setCurrentStep,
+  finalValues,
+  setFinalValues,
+}: PropertiesFormStepProps) {
+  const [tempFiles, setTempFiles] = React.useState<any[]>([]); // [
+  const onFinish = () => {
+    setFinalValues({
+      ...finalValues,
+      media: {
+        newlyUploadedFiles: tempFiles,
+        images: finalValues.media.images,
+      },
+    });
+    setCurrentStep(currentStep + 1);
+  };
   return (
     <div>
-      <span>Media</span>
+      <div className="flex flex-wrap gap-5 mb-5">
+        {finalValues.media.images.map((image: string) => (
+          <div className="flex flex-col gap-3 border border-dashed border-gray-400 p-2 rounded justify-center items-center">
+            <img
+              src={image}
+              alt=""
+              className="w-20 h-20 object-cover rounded"
+            />
+            <span
+              className="text-red-500 underline text-sm cursor-pointer"
+              onClick={() => {
+                let tempMedia = finalValues.media;
+                tempMedia.images = tempMedia.images.filter(
+                  (img: string) => img !== image
+                );
+                setFinalValues({
+                  ...finalValues,
+                  media: {
+                    newlyUploadedFiles: tempFiles,
+                    images: tempMedia.images,
+                  },
+                });
+              }}
+            >
+              Delete
+            </span>
+          </div>
+        ))}
+      </div>
+
+      <Upload
+        listType="picture-card"
+        multiple
+        beforeUpload={(file: any) => {
+          setTempFiles((prev) => [...prev, file]);
+          return false;
+        }}
+      >
+        Upload
+      </Upload>
       <div className="flex justify-end gap-5">
         <Button
-          type="primary"
           onClick={() => setCurrentStep(currentStep - 1)}
         >
           Back
         </Button>
-        <Button type="primary" onClick={() => setCurrentStep(currentStep + 1)}>
+        <Button type="primary" onClick={() => onFinish()}>
           Next
         </Button>
       </div>
     </div>
   );
 }
+
+export default Media;
